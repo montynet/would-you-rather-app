2def8857722e497b1cdb7899fcb32d09825256b3
Roberto Monterrosa <r.c.monterrosa@gmail.com>
2019-07-13 23:38:34 -0700

    Basic structure of the app, added needed dependencies

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// stateless component as it's only a presentational component
+const NotFound = () => {
+  return (
+    <div>
+      Not Found
+    </div>
+  )
+};
+
+export default NotFound
